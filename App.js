0be6eb235bf5d3d435074a0ea99739f8b8bc3df9
Ciprian Turcu <turcuciprian1@gmail.com>
2017-12-02 13:39:04 +0200

    changed car image, added text at the bottom of the car

--- App.js
+++ App.js
@@ -1,26 +1,72 @@
 import React, { Component } from 'react';
-import { AppRegistry, View, Image, StyleSheet } from 'react-native';
+import { AppRegistry, View, Image,Text, StyleSheet,  } from 'react-native';
 const styles = StyleSheet.create({
+  body:{
+
+  },
   car: {
     width: 150,
     height: 300,
   },
-  carContainer: {
+  mainContainer: {
     flex:1,
-    flexDirection:'row',
+    flexDirection:'column',
     alignItems:'center',
       justifyContent:'center'
+  }, 
+  bottomStuff:{
+    flex:1,
+    flexDirection:'column',
+    alignItems:'center',
+      justifyContent:'center',
+      width:100,
+      height:100
+  },
+  bigText:{
+    fontSize:72
+  },
+  smallText:{
+    fontSize: 22
+  },
+  half:{
+    flex:1
   }
 });
-
 export default class DisplayAnImage extends Component {
   render() {
     return (
-      <View style={styles.carContainer}>
+      // <View style={styles.body}>
+      //   <View style={styles.half}>
+      //       <View style={styles.carContainer}>
+      //         <Image
+      //           source={require('./src/img/car.png')}
+      //           style={styles.car}
+      //         />
+      //         </View>
+      //   </View>
+      //   <View style={styles.half}>
+      //     <View style={styles.carContainer}>
+      //     <Text style={styles.bigText}>00</Text>
+      //     <Text style={styles.smallText}>Centimeters</Text>
+      //     </View>
+      //   </View>
+      // </View>
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <View style={{width: 150, height: 300}} >
         <Image
           source={require('./src/img/car.png')}
           style={styles.car}
         />
+        </View>
+        <View>
+        <Text style={styles.bigText}>00</Text>
+            <Text style={styles.smallText}>Centimeters</Text>
+            </View>
       </View>
     );
   }
